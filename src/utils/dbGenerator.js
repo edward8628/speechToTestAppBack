4dedf14bdff02882fb1c1a6b01782b7828fd36be
Feng Liu <fliu@us.ibm.com>
2016-11-06 14:26:47 -0800

    add TODO files for later

--- src/utils/dbGenerator.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const debug = require('debug')('randombackend-dbGenerator');
-
-
-
-
---
+++ src/utils/mongodb.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const debug = require('debug')(`${process.env.APPNAME}:mongodb`);
+
+//TODO
+
