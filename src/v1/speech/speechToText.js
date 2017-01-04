cb4f5fe9e1480d284045a3141319caf9ccf3a469
fliu <fliu@us.ibm.com>
2017-01-03 16:28:32 -0800

    change code to WebSockets

--- src/v1/speech/speechToText.js
+++ src/v1/speech/speechToText.js
@@ -9,33 +9,60 @@ const speech_to_text = new SpeechToTextV1 ({
 const speechToText = (req, res) => {
   debug("speechToText request");
   
+  // var params = {
+  //   audio: fs.createReadStream('audio-file.flac'),
+  //   content_type: 'audio/flac',
+  //   max_alternatives: 3,
+  //   word_confidence: true,
+  // };
+  
+  //   speech_to_text.recognize(params, function(error, transcript) {
+  //     if (error)
+  //       debug('speechToTexterror:', error);
+  //     else {
+  //       debug("speechToText successful");
+  //       // debug(JSON.stringify(transcript, null, 2));
+  //       res.status(200).send({message: transcript.results[0].alternatives[0].transcript});
+  //     }
+  //   });
+  
+  //WebSockets
   var params = {
-    audio: fs.createReadStream('audio-file.flac'),
+    model: 'en-US_BroadbandModel',
     content_type: 'audio/flac',
+    continuous: true,
+    interim_results: true,
     max_alternatives: 3,
-    word_confidence: true,
-    keywords: ['colorado', 'tornado', 'tornadoes'],
-    keywords_threshold: 0.5
+    word_confidence: false,
+    timestamps: false,
   };
   
-  // speech_to_text.getModels(null, function(error, models) {
-  //   if (error)
-  //     debug('error:', error);
-  //   else {
-  //     debug(JSON.stringify(models, null, 2));
-  //     res.status(200).send({success: "ok"});
-  //   }
-  // });
-  
-  speech_to_text.recognize(params, function(error, transcript) {
-    if (error)
-      debug('speechToTexterror:', error);
-    else {
-/*      debug("speechToText successful", JSON.stringify(transcript, null, 2));*/
-      res.status(200).send({message: transcript.results[0].alternatives[0].transcript});
-    }
-  });
+  // Create the stream.
+  var recognizeStream = speech_to_text.createRecognizeStream(params);
 
+  // Pipe in the audio.
+  fs.createReadStream('audio-file.flac').pipe(recognizeStream);
+
+  // Pipe out the transcription to a file.
+  // recognizeStream.pipe(fs.createWriteStream('transcription.txt'));
+
+  // Get strings instead of buffers from 'data' events.
+  recognizeStream.setEncoding('utf8');
+
+  // Listen for events.
+  recognizeStream.on('results', function(event) { onEvent('Results:', event); });
+  recognizeStream.on('data', function(event) { onEvent('Data:', event); });
+  recognizeStream.on('error', function(event) { onEvent('Error:', event); });
+  recognizeStream.on('close', function(event) { onEvent('Close:', event); });
+  recognizeStream.on('speaker_labels', function(event) { onEvent('Speaker_Labels:', event); });
+
+  // Displays events on the console.
+   onEvent = (name, event) => {
+    debug(name, JSON.stringify(event, null, 2));
+    if (name==="Data:") {
+      res.status(200).send({message: event});
+    }
+  };
 }
 
 module.exports = speechToText;
